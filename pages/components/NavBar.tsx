7d3fffa4e481ae578e54b5e4b13649a5306ebe11
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-09-25 10:24:59 -0300

    feat: add NavBar with links to products, login and cart

---
+++ pages/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
+
+export default function NavBar() {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <h3>Allugatv</h3>
+        </Link>
+        <Link href="/">Shop</Link>
+        <ul>
+          <li>
+            <Link href="/login">
+              <FontAwesomeIcon icon={faUser} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/cart">
+              <FontAwesomeIcon icon={faShoppingBag} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import NavBar from "./components/NavBar";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
