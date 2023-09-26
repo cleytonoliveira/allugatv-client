69c11c2a31caa21eeb94fd41b08d4a987fc83c39
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-09-26 11:19:33 -0300

    feat: add page to checkout

---
+++ pages/checkout/index.tsx
@@ -0,0 +1,54 @@
+export default function Checkout() {
+  return (
+    <div>
+      <div>
+        <h1>Checkout</h1>
+        <div>
+          <h3>1. Delivery options</h3>
+          <label htmlFor="fullname">
+            Full name
+            <input type="text" name="name" id="fullname" autoFocus />
+          </label>
+          <label htmlFor="address">
+            Address
+            <input type="text" name="address" id="address" />
+          </label>
+          <label htmlFor="city">
+            City
+            <input type="text" name="city" id="city" />
+          </label>
+          <label htmlFor="postal">
+            Postal code
+            <input type="text" name="lastname" id="postal" />
+          </label>
+        </div>
+        <div>
+          <h3>2. Shipping method</h3>
+          <label htmlFor="standard">
+            <input type="radio" name="delivery" id="standard" />
+            Standard
+          </label>
+          <label htmlFor="express">
+            <input type="radio" name="delivery" id="express" />
+            Express
+          </label>
+        </div>
+        <div>
+          <h3>3. Method of payment</h3>
+          <label htmlFor="credit">
+            <input type="radio" name="payment" id="credit" />
+            Credit card
+          </label>
+          <label htmlFor="paypal">
+            <input type="radio" name="payment" id="paypal" />
+            PayPal
+          </label>
+        </div>
+      </div>
+      <div>
+        <h3>Your cart</h3>
+        <button>COMPLETE ORDER</button>
+      </div>
+    </div>
+  );
+}
